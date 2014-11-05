bf23762d6368e3005d4e6b3bc7ce4ac5f6a94977
Montana Flynn <montana@montanaflynn.me>
2014-11-04 20:29:36 -0800

    Move data converter to it's own node module

--- json-to-other-formats.js
+++ json-to-other-formats.js
@@ -1,37 +1,27 @@
 var fs = require('fs')
+var aoot = require('aoot')
 var data = require('./ebola-outbreak-data.json')
 
-var csvStream = fs.createWriteStream('./ebola-outbreak-data.csv');
-csvStream.write("date, cases, deaths\n")
-for (var i = 0; i < data.length; i++) {
-	var d = data[i]
-	csvStream.write(d.date + ", " + d.cases +  ", " + d.deaths + "\n")
-}
-console.log("CSV file saved")
-csvStream.end();
+fs.writeFile("ebola-outbreak-data.csv", aoot.csv(data), function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("CSV file saved!")
+    }
+}) 
 
+fs.writeFile("ebola-outbreak-data.tsv", aoot.tsv(data), function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("TSV file saved!")
+    }
+}) 
 
-var tsvStream = fs.createWriteStream('./ebola-outbreak-data.tsv');
-tsvStream.write("date\tcases\tdeaths\n")
-for (var i = 0; i < data.length; i++) {
-  var d = data[i]
-  tsvStream.write(d.date + "\t" + d.cases +  "\t" + d.deaths + "\n")
-}
-console.log("TSV file saved")
-tsvStream.end();
-
-
-var xmlStream = fs.createWriteStream('./ebola-outbreak-data.xml');
-xmlStream.write('<?xml version="1.0"?>\n')
-xmlStream.write("<ROWSET>\n")
-for (var i = 0; i < data.length; i++) {
-  var d = data[i]
-  xmlStream.write("\t<ROW>\n")
-  xmlStream.write("\t\t<date>"+d.date+"</date>\n")
-  xmlStream.write("\t\t<cases>"+d.cases+"</cases>\n")
-  xmlStream.write("\t\t<deaths>"+d.deaths+"</deaths>\n")
-  xmlStream.write("\t</ROW>\n")
-}
-xmlStream.write("</ROWSET>\n")
-console.log("XML file saved")
-xmlStream.end();
+fs.writeFile("ebola-outbreak-data.xml", aoot.xml(data), function(err) {
+    if(err) {
+        console.log(err)
+    } else {
+        console.log("XML file saved!")
+    }
+}) 
