fc10f7e9722f73c7c3f582f7bef50121e6176d78
Montana Flynn <montana@montanaflynn.me>
2014-11-12 07:20:26 -0800

    Refactor and clean code

--- update.js
+++ update.js
@@ -2,100 +2,109 @@ var fs = require('fs')
 var unirest = require('unirest')
 var cheerio = require('cheerio')
 var rimraf = require('rimraf')
+var aoot = require('aoot')
 var dpdf = require('download-pdf')
 var pdfUtil = require('pdf-to-text')
 
-// Save to data to multiple formats
-var save = require('./lib/saveData.js')
-
 // Last report in local data
-var currentData = require('./ebola-outbreak-data.json')
-var lastReport = currentData[currentData.length - 1]
+var originalData = require('./ebola-outbreak-data.json')
+var lastReport = originalData[originalData.length - 1]
 var lastReportTimestamp = new Date(lastReport.date).getTime()
 
 // Placeholder for new report data
 var reports = []
 
-// Get all the reports from WHO
+// Get all the PDF reports from the WHO website
 unirest
   .get("http://www.who.int/csr/disease/ebola/situation-reports/en/")
   .end(function(response){
 
-    // Get the response body
-    var body = response.body
+    console.log("Ebola report list received")
 
-    // Send the body to cheerio
-    var $ = cheerio.load(body)
+    // Send the body to cheerio for jquerification
+    var $ = cheerio.load(response.body)
 
-    // Find all the elements we care about in the first .list in the source
+    // Find all the a elements we care about in the first .list 
     var elements = $($("#content .list")[0]).children('li').children('a')
 
-    // Loop over the reports
+    // Loop over the reports backwards
     for (var i = elements.length - 1; i >= 0; i--) {
-      var element = elements[i]
 
-      // Get the date 
+      // Get the date from anchor link text
+      var element = elements[i]
       var date = element.children[0].data
       date = new Date(date.split("-")[1].trim())
+
+      // Save the date in epoch timestamp to compare
       var dateTimestamp = new Date(date).getTime()
 
+      // Check if we have new reports
       if (dateTimestamp > lastReportTimestamp) {
 
         // Get the link
         var link = element.attribs.href.trim()
 
-        // Create the object
-        var obj = {
+        // Add to array
+        reports.push({
           date: date,
           link: link
-        }
-
-        // Add to array
-        reports.push(obj)   
+        })   
 
       }
 
     }
 
+    // Keep going if more reports
     if (reports.length > 0) {
       getReports(reports)
+
+    // If we're up to date
     } else {
-      save(currentData)
+      saveData(originalData)
     }
 
   })
 
+// Get all the new reports
 function getReports(reports) {
+
   var report = reports[0]
-  reports.shift()
-  var link = report.link
-  var date = new Date(report.date).toISOString()
-  unirest
-    .get(link)
-    .end(function(response){
-      var body = response.body
-      var opts = { directory : "temp" }
-      console.log("Downloading " + link)
-      dpdf(link, opts, function(err, path){
-        if (err) throw err
-        parseReport(path, function(data){
-          var obj = {
-            date: date,
-            cases: parseInt(data.cases),
-            deaths: parseInt(data.deaths)
-          }
-          currentData.push(obj)
-          if (reports.length > 0) {
-            getReports(reports) 
-          } else {
-            rimraf('./temp', function(err) {
-              if (err) throw err
-              save(currentData)
-            })
-          }
-        })
+  console.log("Downloading " + report.link)
+
+  // Save the pdf report into the temp directory
+  dpdf(report.link, { directory : "temp" }, function(err, path){
+
+    if (err) throw err
+
+    // Parse the report that was just saved
+    parseReport(path, function(data){
+
+      // Add to the original data array
+      originalData.push({
+        date: new Date(report.date).toISOString(),
+        cases: parseInt(data.cases),
+        deaths: parseInt(data.deaths)
       })
+
+      // Check if more reports to do
+      reports.shift()
+      if (reports.length > 0) {
+
+        // Keep on going
+        getReports(reports) 
+
+      } else {
+
+        // Delete the temp dir
+        rimraf('./temp', function(err) {
+          if (err) throw err
+
+          // Save the data in a bunch of formats
+          saveData(originalData)
+        })
+      }
     })
+  })
 }
 
 function parseReport(report, callback) {
@@ -104,6 +113,7 @@ function parseReport(report, callback) {
 
     if (err) throw(err)
 
+    // Let's hope this doesn't change :/
     var cases = /There have been \S*(?:\s\S+)?/
     var deaths = /with \S*(?:\s\S+)?/
 
@@ -124,4 +134,48 @@ function parseReport(report, callback) {
     callback(obj)
 
   })
+}
+
+function saveData(data) {
+    
+  fs.writeFile("./ebola-outbreak-data.json", JSON.stringify(data,null,4), function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("JSON file saved!")
+    }
+  }) 
+
+  fs.writeFile("./ebola-outbreak-data.csv", aoot.csv(data), function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("CSV file saved!")
+    }
+  }) 
+
+  fs.writeFile("./ebola-outbreak-data.tsv", aoot.tsv(data), function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("TSV file saved!")
+    }
+  }) 
+
+  fs.writeFile("./ebola-outbreak-data.xml", aoot.xml(data), function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("XML file saved!")
+    }
+  }) 
+
+  fs.writeFile("./ebola-outbreak-data.yaml", aoot.yaml(data), function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      console.log("YAML file saved!")
+    }
+  }) 
+
 }
